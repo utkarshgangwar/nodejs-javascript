af8f9cb465dba0ceea5bf323ef362df79721c00f
utkarshgangwar <utkarshgangwar22@gmail.com>
2026-03-31 17:58:05 +0530

    rate limiter with redis

--- express/index.js
+++ express/index.js
@@ -1,44 +1,76 @@
 const express = require("express");
+const { createClient } = require("redis");
 
 const app = express();
+const redisClient = createClient();
 
-const store = new Map();
+redisClient.on("error", (err) => console.log("Redis Error", err));
+
+(async () => {
+  await redisClient.connect();
+})();
+
+// const store = new Map();
 const Rate_Limit = 5;
 const window_size = 60 * 1000;
 
-function rateLimiter(req, res, next) {
-  const userIp = req.ip;
-  const currentTime = Date.now();
+// function rateLimiter(req, res, next) {
+//   const userIp = req.ip;
+//   const currentTime = Date.now();
 
-  if (!store.has(userIp)) {
-    store.set(userIp, {
-      count: 1,
-      startTime: currentTime,
-    });
-    return next();
-  }
+//   if (!store.has(userIp)) {
+//     store.set(userIp, {
+//       count: 1,
+//       startTime: currentTime,
+//     });
+//     return next();
+//   }
 
-  const data = store.get(userIp);
-  const timeDiff = currentTime - data.startTime;
+//   const data = store.get(userIp);
+//   const timeDiff = currentTime - data.startTime;
 
-  if (timeDiff < window_size) {
-    data.count++;
-    if (data.count > Rate_Limit) {
+//   if (timeDiff < window_size) {
+//     data.count++;
+//     if (data.count > Rate_Limit) {
+//       return res.status(429).json({
+//         message: "Too many requests",
+//       });
+//     }
+//   } else {
+//     store.set(userIp, {
+//       count: 1,
+//       startTime: currentTime,
+//     });
+//   }
+
+//   next();
+// }`
+
+async function rateLimiterWithRedis(req, res, next) {
+  try {
+    const userIp = req.ip;
+
+    const requests = await redisClient.incr(userIp);
+
+    if (requests === 1) {
+      await redisClient.expire(userIp, window_size);
+    }
+
+    if (requests > Rate_Limit) {
       return res.status(429).json({
         message: "Too many requests",
       });
     }
-  } else {
-    store.set(userIp, {
-      count: 1,
-      startTime: currentTime,
-    });
-  }
 
-  next();
+    next();
+  } catch (error) {
+    console.log(error);
+    next();
+  }
 }
 
-app.use(rateLimiter);
+// app.use(rateLimiter);
+app.use(rateLimiterWithRedis);
 
 app.get("/", (req, res) => {
   res.status(200).json("Hello World");
